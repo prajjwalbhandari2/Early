38d809a7b0c8eb8fd99fd7c01029ed7cb6608332
Prajjwal <bhandariprajjwal123@gmail.com>
2022-07-19 14:04:14 +0545

    New File

---
+++ validate_input.py
@@ -0,0 +1,19 @@
+def user_choice():
+    choice = 'Wrong'
+    acceptable_range = range(0,11)
+    within_range = False
+
+    while choice.isdigit() == False or within_range == False:
+        choice = input('Enter a number (0-10):')
+
+        if choice.isdigit() == False:
+            print('Sorry that is not a digit!')
+        
+        if choice.isdigit() == True:
+            if int(choice) in acceptable_range:
+                within_range = True
+            else:
+                print('Sorry, you are out of acceptable range!')
+                within_range = False
+    return int(choice)
+print(user_choice())
